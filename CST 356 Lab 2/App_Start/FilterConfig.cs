4c728298dd0ac946d55f220d383f2072a64c72de
Nicholas Olson <latentdev@gmail.com>
2018-01-29 18:17:57 -0800

    Add project files.

---
+++ CST 356 Lab 2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CST_356_Lab_2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CST 356 Lab 2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CST_356_Lab_2.MvcApplication" Language="C#" %>
